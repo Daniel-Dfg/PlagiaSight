10a790c5c0c69ed713393a96bdaa065b2ad2bccc
LUCKYINS <elhusseinabdalrahman@gmail.com>
2024-09-07 23:56:05 +0200

    megaSearch init
    - solution for http many request error

---
+++ src/megasearch.py
@@ -0,0 +1,36 @@
+from duckduckgo_search import DDGS
+from googlesearch import search
+from time import sleep
+from random import randint
+
+def megaSearch(word_sent, number, headers=None, start=0):
+    while True:
+        try:
+            ddgo = DDGS(headers=headers)
+            results = ddgo.text(word_sent, max_results=number)
+            urls = [result['href'] for result in results][start:]
+            sleep(2)
+            return urls
+        except:
+            print("1 donwn")  
+        try:
+            urls = list(search(word_sent, num=number, stop=number, start=start, pause=2, user_agent=headers["User-Agent"]))
+            return urls
+        except:
+            givePenalty()
+
+def givePenalty():
+    print("5 min penalty")
+    sleep(20)
+    print("Nah...")
+    sleep(20)
+    print("You're kidding right?!")
+    sleep(20)
+    print("This is search engines a abuse...")
+    sleep(60)
+    print("What are you even searching?")
+    sleep(60)
+    print("A Whale?!")
+    sleep(60)
+    print("Be patient only one minute")
+    sleep(60)
