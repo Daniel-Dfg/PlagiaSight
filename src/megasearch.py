89cf83f936166080baad68b715797678900007a6
LUCKYINS <elhusseinabdalrahman@gmail.com>
2024-09-08 13:34:16 +0200

    Update
    - Slow but working web scraping

--- src/megasearch.py
+++ src/megasearch.py
@@ -3,21 +3,24 @@ from googlesearch import search
 from time import sleep
 from random import randint
 
-def megaSearch(word_sent, number, headers=None, start=0):
+def megaSearch(word_sent, number,cooldown, headers=None, start=0):
     while True:
         try:
-            ddgo = DDGS(headers=headers)
-            results = ddgo.text(word_sent, max_results=number)
-            urls = [result['href'] for result in results][start:]
-            sleep(2)
-            return urls
+            if not cooldown[0]:
+                ddgo = DDGS(headers=headers)
+                results = ddgo.text(word_sent, max_results=number)
+                urls = [result['href'] for result in results][start:]
+                sleep(2)
+                return urls
         except:
             print("1 donwn")  
+            cooldown[0] = True
         try:
             urls = list(search(word_sent, num=number, stop=number, start=start, pause=2, user_agent=headers["User-Agent"]))
             return urls
         except:
             givePenalty()
+            cooldown[0] = False
 
 def givePenalty():
     print("5 min penalty")
