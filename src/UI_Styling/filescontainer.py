606e1530dcf4526b781e75f30a8462b8dbe0b9fe
LUCKYINS <148013915+LUCKYINS@users.noreply.github.com>
2024-10-31 20:13:05 +0100

    Update filescontainer.py

--- src/UI_Styling/filescontainer.py
+++ src/UI_Styling/filescontainer.py
@@ -1,4 +1,17 @@
-from PySide6.QtWidgets import QWidget, QWidgetItem
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QVector2D
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QWidget, QWidgetItem
+from UI_Styling.slistwidget import SListWidget
+w = 500
+h = 340
+
 class FilesContainer(QWidget):
     def __init__(self) -> None:
         super().__init__()
+        self.setFixedSize(w, h)
+        self.main_layout = QVBoxLayout(self)
+        self.validContainer = SListWidget()
+        self.invalidContainer = SListWidget()
+        self.invalidContainer.label.setText("InValid Files:")
+        self.main_layout.addWidget(self.validContainer, alignment=Qt.AlignmentFlag.AlignTop)
+        self.main_layout.addWidget(self.invalidContainer, alignment=Qt.AlignmentFlag.AlignBottom)
